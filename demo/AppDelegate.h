70e95569ed5d3a5c493cece0c48245c2f5b2e7d9
Verma Mukesh <vermamukesh67@gmail.com>
2018-04-27 01:41:39 +0530

    first commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by Verma Mukesh on 25/04/18.
+//  Copyright © 2018 Verma Mukesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/SecondViewController.h
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.h
+//  demo
+//
+//  Created by Verma Mukesh on 25/04/18.
+//  Copyright © 2018 Verma Mukesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *jkjkjfdd;
+@property(nonatomic, weak) NSMutableDictionary *diccRef;
+@end
